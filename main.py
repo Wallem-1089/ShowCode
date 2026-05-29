6980b5c7b7b3a6c2bc3ae29edf9704c9e6273808
Wallem-1089 <73552998+Wallem-1089@users.noreply.github.com>
2026-05-29 21:07:31 +0100

    added files

---
+++ main.py
@@ -0,0 +1,5 @@
+# To open another python file from this python script
+import subprocess
+
+subprocess.Popen(['C:\\Users\\<YOUR USERNAME>\\AppData\\Local\\Programs\\Python\\Python38\\python.exe', 'hello.py'])
+# To display the code of another python script
